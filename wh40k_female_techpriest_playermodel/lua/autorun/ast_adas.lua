5c70a5e19602e5cd187d458fbc34d832a884537d
AsylumReaper <gorgeousdragon88@gmail.com>
2026-07-29 16:09:23 -0500

    Delete wh40k_female_techpriest_playermodel directory

--- wh40k_female_techpriest_playermodel/lua/autorun/ast_adas.lua
+++
@@ -1,3 +0,0 @@
-player_manager.AddValidModel(   "mech_fem_tech", "models/astartes/adas/mech_fem_tech.mdl" )
-list.Set( "PlayerOptionsModel", "mech_fem_tech", "models/astartes/adas/mech_fem_tech.mdl" )
-player_manager.AddValidHands(   "mech_fem_tech", "models/astartes/adas/hand/mech_fem_tech_carms.mdl", 0, "10000000" )
